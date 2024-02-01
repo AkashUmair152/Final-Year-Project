85d1e20b6761ce938d6a533ba738f99b581e337c
Akash-Umair <83009456+AkashUmair152@users.noreply.github.com>
2024-02-01 22:51:49 +0500

    rest the subscribe section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,10 +13,10 @@ export default function Home() {
     <Header/>
     <Hero/>
     <Content/>
-    <Subscribe/>
     <Gallery/>
     <CompanyIcon/>
     <Team/>
+    <Subscribe/>
     <Footer/>
     
     </>
