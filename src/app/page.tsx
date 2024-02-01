f135aea18288173a60607013585ed9130750ed65
Akash-Umair <83009456+AkashUmair152@users.noreply.github.com>
2024-02-01 15:42:18 +0500

    companyIcon section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
 
-import Category from "./components/Category";
 import Hero from "./components/Hero";
 import Content from "./components/Content";
 import Team from "./components/Team";
 import Subscribe from "./components/Subscribe";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import CompanyIcon from "./components/CompanyIcon";
 export default function Home() {
   return (
     <>
     <Header/>
     <Hero/>
-    <Category/>
     <Content/>
     <Subscribe/>
+    <CompanyIcon/>
     <Team/>
     <Footer/>
     
